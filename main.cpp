0c203aec4d427738e78be43928247bbe69a79091
kosdjs <kani39@naver.com>
2016-10-09 16:55:46 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2016. 10. 9.
+ *      Author: song
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct student
+{
+	int id;
+	string name;
+	int grade;
+};
+
+int main()
+{
+	int i;
+	int top=0;
+	struct student s[10];
+	for(i=0;i<10;i++)
+	{
+		cin>>s[i].id>>s[i].name>>s[i].grade;
+	}
+	for(i=0;i<10;i++)
+	{
+		if(s[i].grade>s[top].grade)
+		{
+			top=i;
+		}
+	}
+	cout<<"학번 : "<<s[top].id<<endl;
+	cout<<"이름 : "<<s[top].name<<endl;
+	cout<<"성적 : "<<s[top].grade<<endl;
+
+	return 0;
+}
